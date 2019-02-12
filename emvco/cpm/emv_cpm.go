08b8673c22a1c0ca226fefba3285146e96ceac02
Dongri Jin <dongrify@gmail.com>
2019-02-13 00:42:51 +0900

    WIP

--- emvco/cpm/emv_cpm.go
+++ emvco/cpm/emv_cpm.go
@@ -1,48 +1,167 @@
 package cpm
 
 import (
+	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"strconv"
 	"strings"
 	"unicode/utf8"
 )
 
-// const ....
+// const ...
 const (
-	IDPayloadFormatIndicator = "00" // (M) Payload Format Indicator
+	IDPayloadFormatIndicator                 = "85" // (M) Payload Format Indicator
+	IDApplicationTemplate                    = "61" // (M) Application Template
+	IDCommonDataTemplate                     = "62" // (O) Common Data Template
+	IDApplicationSpecificTransparentTemplate = "63" // (O) Application Specific Transparent Template
+	IDCommonDataTransparentTemplate          = "64" // (O) Common Data Transparent Template
 )
 
-// Data Objects for Additional Data Field Template (ID "62")
+// const ...
 const (
-	AdditionalIDBillNumber = "01"
-)
-
-// Data Objects for Merchant Information—Language Template (ID "64")
-const (
-	MerchantInformationIDLanguagePreference = "00"
+	TagApplicationDefinitionFileName = "4F" // Application Definition File (ADF) Name
+	TagApplicationLabel              = "50"
+	TagTrack2EquivalentData          = "57"
+	TagApplicationPAN                = "5A"
+	TagCardholderName                = "5F20"
+	TagLanguagePreference            = "5F2D"
+	TagIssuerURL                     = "5F50"
+	TagApplicationVersionNumber      = "9F08"
+	TagTokenRequestorID              = "9F19"
+	TagPaymentAccountReference       = "9F24"
+	TagLast4DigitsOfPAN              = "9F25"
 )
 
 // EMVQR ...
 type EMVQR struct {
-	PayloadFormatIndicator string
+	DataPayloadFormatIndicator string              // 85
+	ApplicationTemplate        ApplicationTemplate // 61
+	CommonDataTemplate         CommonDataTemplate  // 62
+}
+
+// ApplicationTemplate ...
+type ApplicationTemplate struct {
+	BERTLV
+	// ApplicationSpecificTransparentTemplate ApplicationSpecificTransparentTemplate // 63
+}
+
+// CommonDataTemplate ...
+type CommonDataTemplate struct {
+	CommonDataTransparentTemplate CommonDataTransparentTemplate // 64
+}
+
+// CommonDataTransparentTemplate ...
+type CommonDataTransparentTemplate struct {
+	BERTLV
+}
+
+// ApplicationSpecificTransparentTemplate ...
+type ApplicationSpecificTransparentTemplate struct {
+	BERTLV
+}
+
+// BERTLV ...
+type BERTLV struct {
+	DataApplicationDefinitionFileName string // "4F"
+	DataApplicationLabel              string // "50"
+	DataTrack2EquivalentData          string // "57"
+	DataApplicationPAN                string // "5A"
+	DataCardholderName                string // "5F20"
+	DataLanguagePreference            string // "5F2D"
+	DataIssuerURL                     string // "5F50"
+	DataApplicationVersionNumber      string // "9F08"
+	DataTokenRequestorID              string // "9F19"
+	DataPaymentAccountReference       string // "9F24"
+	DataLast4DigitsOfPAN              string // "9F25"
 }
 
 // GeneratePayload ...
 func (c *EMVQR) GeneratePayload() (string, error) {
 	s := ""
-	if c.PayloadFormatIndicator != "" {
-		s += format(IDPayloadFormatIndicator, c.PayloadFormatIndicator)
+	if c.DataPayloadFormatIndicator != "" {
+		s += format(IDPayloadFormatIndicator, toHex(c.DataPayloadFormatIndicator))
 	} else {
-		return "", fmt.Errorf("PayloadFormatIndicator is mandatory")
+		return "", fmt.Errorf("DataPayloadFormatIndicator is mandatory")
 	}
+
+	if (ApplicationTemplate{}) != c.ApplicationTemplate {
+		//t := c.ApplicationTemplate
+		tt := c.ApplicationTemplate
+		template := ""
+		if tt.DataApplicationDefinitionFileName != "" {
+			template += format(TagApplicationDefinitionFileName, toBinary(tt.DataApplicationDefinitionFileName))
+		}
+		if tt.DataApplicationLabel != "" {
+			template += format(TagApplicationLabel, toHex(tt.DataApplicationLabel))
+		}
+		if tt.DataTrack2EquivalentData != "" {
+			template += format(TagTrack2EquivalentData, toBinary(tt.DataTrack2EquivalentData))
+		}
+		if tt.DataApplicationPAN != "" {
+			template += format(TagApplicationPAN, tt.DataApplicationPAN)
+		}
+		if tt.DataCardholderName != "" {
+			template += format(TagCardholderName, toHex(tt.DataCardholderName))
+		}
+		if tt.DataLanguagePreference != "" {
+			template += format(TagLanguagePreference, toHex(tt.DataLanguagePreference))
+		}
+		if tt.DataIssuerURL != "" {
+			template += format(TagIssuerURL, toHex(tt.DataIssuerURL))
+		}
+		if tt.DataApplicationVersionNumber != "" {
+			template += format(TagApplicationVersionNumber, toHex(tt.DataApplicationVersionNumber))
+		}
+		if tt.DataTokenRequestorID != "" {
+			template += format(TagTokenRequestorID, toHex(tt.DataTokenRequestorID))
+		}
+		if tt.DataPaymentAccountReference != "" {
+			template += format(TagPaymentAccountReference, toHex(tt.DataPaymentAccountReference))
+		}
+		if tt.DataLast4DigitsOfPAN != "" {
+			template += format(TagLast4DigitsOfPAN, toHex(tt.DataLast4DigitsOfPAN))
+		}
+		//fmt.Println(template)
+		//ts := format(IDApplicationSpecificTransparentTemplate, template)
+		//fmt.Println(ts)
+		s += format(IDApplicationTemplate, template)
+		fmt.Println(s)
+	}
+	decoded, err := hex.DecodeString(s)
+	if err != nil {
+		return "", err
+	}
+	s = base64.StdEncoding.EncodeToString([]byte(string(decoded)))
 	return s, nil
 }
 
 func format(id, value string) string {
+	length := utf8.RuneCountInString(value) / 2
+	lengthStr := strconv.Itoa(length)
+	fmt.Println(lengthStr)
+	fmt.Printf("%X", lengthStr)
+	lengthStr = "00" + fmt.Sprintf("%X", length)
+	return id + lengthStr[len(lengthStr)-2:] + value
+}
+
+func formatStr(id, value string) string {
 	length := utf8.RuneCountInString(value)
 	lengthStr := strconv.Itoa(length)
 	lengthStr = "00" + lengthStr
 	return id + lengthStr[len(lengthStr)-2:] + value
+}
+
+func toBinary(s string) string {
+	return s
+}
+
+func toHex(s string) string {
+	src := []byte(s)
+	dst := make([]byte, hex.EncodedLen(len(src)))
+	hex.Encode(dst, src)
+	return string(dst)
+	// fmt.Printf("%s\n", dst)
 }
 
 func formatAmount(amount float64) string {
--- main.go
+++ main.go
@@ -1,34 +1,47 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
-	"github.com/dongri/emvco-qrcode/emvco"
+	"github.com/dongri/emvco-qrcode/emvco/cpm"
 )
 
 func main() {
-	emvqr := new(emvco.EMVQR)
-	emvqr.PayloadFormatIndicator = "01"
-	emvqr.PointOfInitiationMethod = "12" // 11 is static qrcode
-	emvqr.MerchantAccountInformation = "ABCDEF1234567890"
-	emvqr.MerchantCategoryCode = "5311"
-	emvqr.TransactionCurrency = "392"
-	emvqr.TransactionAmount = 999
-	emvqr.CountryCode = "JP"
-	emvqr.MerchantName = "DONGRI"
-	emvqr.MerchantCity = "TOKYO"
+	// emvqr := new(emvco.EMVQR)
+	// emvqr.PayloadFormatIndicator = "01"
+	// emvqr.PointOfInitiationMethod = "12" // 11 is static qrcode
+	// emvqr.MerchantAccountInformation = "ABCDEF1234567890"
+	// emvqr.MerchantCategoryCode = "5311"
+	// emvqr.TransactionCurrency = "392"
+	// emvqr.TransactionAmount = 999
+	// emvqr.CountryCode = "JP"
+	// emvqr.MerchantName = "DONGRI"
+	// emvqr.MerchantCity = "TOKYO"
 
-	additionalTemplate := new(emvco.AdditionalDataFieldTemplate)
-	additionalTemplate.BillNumber = "hoge"
-	additionalTemplate.ReferenceLabel = "fuga"
-	additionalTemplate.TerminalLabel = "piyo"
+	// additionalTemplate := new(emvco.AdditionalDataFieldTemplate)
+	// additionalTemplate.BillNumber = "hoge"
+	// additionalTemplate.ReferenceLabel = "fuga"
+	// additionalTemplate.TerminalLabel = "piyo"
 
-	emvqr.AdditionalDataFieldTemplate = *additionalTemplate
+	// emvqr.AdditionalDataFieldTemplate = *additionalTemplate
 
-	qrcodeData, err := emvqr.GeneratePayload()
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
-	log.Println(qrcodeData)
+	// qrcodeData, err := emvqr.GeneratePayload()
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// 	return
+	// }
+	// log.Println(qrcodeData)
+
+	qr := new(cpm.EMVQR)
+	qr.DataPayloadFormatIndicator = "CPV01"
+
+	template := new(cpm.ApplicationTemplate)
+	template.DataApplicationDefinitionFileName = "A0000000555555"
+	template.DataTrack2EquivalentData = "1234567890123458D191220112345F"
+
+	qr.ApplicationTemplate = *template
+	qrcode, err := qr.GeneratePayload()
+	fmt.Println(err)
+	fmt.Println(qrcode)
+
 }
