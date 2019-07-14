277b7b1b42ba465f718c8dd2eea47a57614ac026
Dongri Jin <dongrify@gmail.com>
2019-07-14 17:47:55 +0900

    Fix sample

--- main.go
+++ main.go
@@ -34,26 +34,26 @@ func main() {
 	additionalTemplate.SetReferenceLabel("fuga")
 	additionalTemplate.SetTerminalLabel("piyo")
 	emvqr.SetAdditionalDataFieldTemplate(additionalTemplate)
-	mpmQRCode, err := mpm.Encode(emvqr)
+	code, err := mpm.Encode(emvqr)
 	if err != nil {
 		log.Println(err.Error())
 		return
 	}
-	log.Println(mpmQRCode)
+	log.Println(code)
 	// 0002010102121313JCB12345678900416MASTER12345678905204531153033925407999.1235802JP5906DONGRI6005TOKYO62240104hoge0504fuga0704piyo6304C343
 
 	// MPM Parse
-	emvQR, err := mpm.Decode("00020101021229300012D156000000000510A93FO3230Q31280012D15600000001030812345678520441115802CN5914BEST TRANSPORT6007BEIJING64200002ZH0104最佳运输0202北京540523.7253031565502016233030412340603***0708A60086670902ME91320016A0112233449988770708123456786304A13A")
+	emvqr, err = mpm.Decode("00020101021229300012D156000000000510A93FO3230Q31280012D15600000001030812345678520441115802CN5914BEST TRANSPORT6007BEIJING64200002ZH0104最佳运输0202北京540523.7253031565502016233030412340603***0708A60086670902ME91320016A0112233449988770708123456786304A13A")
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	log.Println(emvQR)
+	log.Println(emvqr)
 
-	binary := emvQR.BinaryData()
+	binary := emvqr.BinaryData()
 	log.Println("\n" + binary)
 
-	json := emvQR.JSON()
+	json := emvqr.JSON()
 	log.Println(json)
 
 	// CPM
